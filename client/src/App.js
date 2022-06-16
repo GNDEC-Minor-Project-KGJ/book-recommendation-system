221d372e5954098c17f28ce6ba98c952a2b231f3
Konark Lohat <konarklohat123456@gmail.com>
2022-06-17 01:58:34 +0530

    Finalized UI

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import {
   CardBody,
   CardFooter,
   Typography,
+  Select,
+  Option,
+  Radio,
 } from "@material-tailwind/react";
 import "./App.css";
 
@@ -54,6 +57,12 @@ function App() {
 
   return (
     <div className="App">
+      <div className="my-5">
+        <Typography variant="h1" color="blue-grey">
+          Book Recommendation System
+        </Typography>
+      </div>
+
       <div className="m-6 p-6 flex flex-col justify-center items-center">
         <Input
           size="lg"
@@ -81,35 +90,47 @@ function App() {
           </Button>
         </div>
       </div>
-      <div className="m-6 p-6 flex flex-row flex-basis-auto justify-center items-center flex-wrap">
-        {books && books.map((book) => (
-          <Card className="w-96 m-6">
-            <CardHeader color="blue" className="relative h-56">
-              <img
-                src={book.url}
-                alt="img-blur-shadow"
-                className="w-full h-full"
-              />
-            </CardHeader>
-            <CardBody className="text-center">
-              <Typography variant="h5" className="mb-2">
-                {book.title}
-              </Typography>
-              <Typography>{book.desc.substr(0, 300) + "..."}</Typography>
-            </CardBody>
-            <CardFooter
-              divider
-              className="flex items-center justify-between py-3"
-            >
-              <Typography variant="small">{book.genre}</Typography>
-              <Typography variant="small" color="grey" className="flex gap-1">
-                <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-                {book.author}
-              </Typography>
-            </CardFooter>
-          </Card>
-        ))}
+      <div className="m-6 p-6  flex flex-row flex-basis-auto justify-center items-center flex-wrap">
+        {books &&
+          books.map((book, index) => (
+            <Card key={index} className="w-96 m-6">
+              <CardHeader color="blue" className="relative h-56">
+                <img
+                  src={book.url}
+                  alt="img-blur-shadow"
+                  className="w-full h-full"
+                />
+              </CardHeader>
+              <CardBody className="text-center">
+                <Typography variant="h5" className="mb-2">
+                  {book.title}
+                </Typography>
+                <Typography>{book.desc.substr(0, 300) + "..."}</Typography>
+              </CardBody>
+              <CardFooter
+                divider
+                className="flex items-center justify-between py-3"
+              >
+                <Typography variant="small">{book.genre}</Typography>
+                <Typography variant="small" color="grey" className="flex gap-1">
+                  <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
+                  {book.author}
+                </Typography>
+              </CardFooter>
+            </Card>
+          ))}
       </div>
+      <footer className="w-full fixed bottom-0 h-12 bg-gray-900">
+        <Typography
+          color="white"
+          variant="small"
+          className="h-full flex items-center justify-center"
+        >
+          Guru Nanak Dev Engineering College
+          <br />
+          Minor Project by Group 22
+        </Typography>
+      </footer>
     </div>
   );
 }
